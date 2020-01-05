78543bf0b1acaa597c7e0be40a05acd1db8a2cb6
thanhcongnguyen <thanhcong150593@gmail.com>
2020-01-05 18:16:30 +0700

    enable route

--- src/routes/index.route.js
+++ src/routes/index.route.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 //import routes
 const usersRouter = require('./user.route');
-// const postRouter = require('./post.route');
+const postRouter = require('./post.route');
 
 router.use('/user', usersRouter);
-// router.use('/post', postRouter);
+router.use('/post', postRouter);
 
 module.exports = router;
